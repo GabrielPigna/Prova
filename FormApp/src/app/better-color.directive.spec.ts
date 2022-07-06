c623548acf3821dcb4679bdd819824d2be94aea6
gabriel pignatiello <gabrielpigna@gmail.com>
2022-07-06 12:22:11 +0200

    first commit

---
+++ FormApp/src/app/better-color.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BetterColorDirective } from './better-color.directive';
+
+describe('BetterColorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BetterColorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
